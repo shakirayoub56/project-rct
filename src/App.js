c19c1fc1df0d306eaffc498fad7b5c65eb109733
Shakir <117161359+shakirayoub123@users.noreply.github.com>
2023-12-19 18:07:13 +0530

    new-routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'; // Changed the import structure
+
+import ShopCategory from './pages/ShopCategory';
+import Shop from './pages/Shop';
+import LoginSignup from './pages/LoginSignup';
+import Product from './pages/Product';
+import Cart from './pages/Cart';
 
 function App() {
   return (
-  <Navbar />
+    <div>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Shop />} />
+          <Route path='/mens' element={<ShopCategory category="men" />} />
+          <Route path='/womens' element={<ShopCategory category="women" />} />
+          <Route path='/kids' element={<ShopCategory category="kid" />} />
+          <Route path="/product/:productId" element={<Product />} /> {/* Changed the path */}
+          <Route path='/cart' element={<Cart />} />
+          <Route path='/login' element={<LoginSignup />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
