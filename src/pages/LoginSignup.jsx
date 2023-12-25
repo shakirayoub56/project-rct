99a044569dd07fbc3251c3e8b816dc965381f19a
Shakir <117161359+shakirayoub123@users.noreply.github.com>
2023-12-25 16:45:07 +0530

    new-fix

--- src/pages/LoginSignup.jsx
+++ src/pages/LoginSignup.jsx
@@ -1,26 +1,70 @@
-import React from 'react'
-import './pagesCss/LoginSignup.css'
+import React, { useState } from 'react';
+import Swal from 'sweetalert2';
+import './pagesCss/LoginSignup.css';
 
 const LoginSignup = () => {
+  const [showLogin, setShowLogin] = useState(false);
+
+  const handleSignup = (e) => {
+    e.preventDefault();
+    // Placeholder logic for signup success
+    Swal.fire({
+      icon: 'success',
+      title: 'Signed up successfully!',
+      text: 'You have successfully signed up.',
+      showConfirmButton: false,
+      timer: 2000,
+    });
+  };
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    // Placeholder logic for login success
+    Swal.fire({
+      icon: 'success',
+      title: 'Logged in successfully!',
+      text: 'You have successfully logged in.',
+      showConfirmButton: false,
+      timer: 2000,
+    });
+  };
+
+  const toggleForm = () => {
+    setShowLogin((prev) => !prev);
+  };
+
   return (
     <div className='loginsignup'>
       <div className="loginsignup-container">
-        <h1>Sign Up</h1>
-        <div className="loginsignup-fields">
-          <input type="text" placeholder='Your Name' />
-          <input type="email" placeholder='Email Address' />
-          <input type="password" placeholder='Password' />
-        </div>
-        <button>Continue</button>
-        <p className="loginsignup-login">Already have an account? <span>Login here</span></p>
+        <h1>{showLogin ? 'Login' : 'Sign Up'}</h1>
+        {showLogin ? (
+          <form onSubmit={handleLogin}>
+            <div className="loginsignup-fields">
+              <input type="text" placeholder='Username' required />
+              <input type="password" placeholder='Password' required />
+            </div>
+            <button type="submit">Login</button>
+          </form>
+        ) : (
+          <form onSubmit={handleSignup}>
+            <div className="loginsignup-fields">
+              <input type="text" placeholder='Your Name' required />
+              <input type="email" placeholder='Email Address' required />
+              <input type="password" placeholder='Password' required />
+            </div>
+            <button type="submit">Continue</button>
+          </form>
+        )}
+        <p className="loginsignup-login" onClick={toggleForm}>
+          {showLogin ? 'Sign Up here' : 'Already have an account? Login here'}
+        </p>
         <div className="loginsignup-agree">
-          <input type="checkbox" name='' id=''/>
-          <p>By continuing , i agree of use terms and policy.</p>
+          <input type="checkbox" name='' id='' required />
+          <p>By continuing, I agree to the terms and policy.</p>
         </div>
       </div>
-      
     </div>
-  )
-}
+  );
+};
 
-export default LoginSignup
+export default LoginSignup;
